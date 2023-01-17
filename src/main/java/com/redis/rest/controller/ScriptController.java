989817694acaac2f4b896b325d1522fd4f018add
yaodong <yaodong199@icloud.com>
2023-01-17 17:04:51 +0800

    lua

--- src/main/java/com/redis/rest/controller/ScriptController.java
+++ src/main/java/com/redis/rest/controller/ScriptController.java
@@ -39,7 +39,7 @@ public class ScriptController {
         // 执行 lua 脚本
         DefaultRedisScript<Boolean> redisScript = new DefaultRedisScript<>();
         // 指定 lua 脚本
-        redisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource("/script/DelKey.lua")));
+        redisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource("/script/del_key.lua")));
         // 指定返回类型
         redisScript.setResultType(Boolean.class);
         System.err.println(redisScript.getScriptAsString());
@@ -56,7 +56,7 @@ public class ScriptController {
         // 执行 lua 脚本
         DefaultRedisScript<Long> redisScript = new DefaultRedisScript<>();
         // 指定 lua 脚本
-        redisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource("/script/CounterLimit.lua")));
+        redisScript.setScriptSource(new ResourceScriptSource(new ClassPathResource("/script/counter_limit.lua")));
         System.err.println(redisScript.getScriptAsString());
         // 指定返回类型
         redisScript.setResultType(Long.class);
