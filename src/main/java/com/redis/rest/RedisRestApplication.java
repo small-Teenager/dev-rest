8d6a5fd92c526e7310d3696ea77cf2f27a59c26b
yaodong <yaodong199@icloud.com>
2023-02-21 17:03:09 +0800

    修改

--- src/main/java/com/redis/rest/RedisRestApplication.java
+++ src/main/java/com/redis/rest/RedisRestApplication.java
@@ -1,13 +1,20 @@
 package com.redis.rest;
 
+import com.redis.rest.utils.IdWorker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RedisRestApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedisRestApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorker() {
+        return new IdWorker();
     }
 
 }
