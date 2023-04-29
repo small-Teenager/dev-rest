fd209768ac1cb0a446379b7b0728c810c6c0aaf3
Yaodong Zhang <yaodong199@icloud.com>
2023-04-29 10:34:25 +0800

    集成jwt

---
+++ src/main/java/com/redis/rest/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.redis.rest.config;
+
+import com.redis.rest.interceptor.TokenInterceptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Autowired
+    private TokenInterceptor tokenInterceptor ;
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(tokenInterceptor)
+                .excludePathPatterns("/apply/user/smsCode","/apply/user/login")
+                .addPathPatterns("/apply/user/code/*")
+        ;
+    }
+}
