54f0e21abc7b305c36b1c3b14889fb7c8675995f
Yaodong Zhang <yaodong199@icloud.com>
2023-09-29 12:52:30 +0800

    .

--- src/main/java/com/dev/rest/response/ApiResponse.java
+++ src/main/java/com/dev/rest/response/ApiResponse.java
@@ -1,6 +1,8 @@
 package com.dev.rest.response;
 
 
+import com.alibaba.fastjson.JSON;
+
 import java.io.Serializable;
 
 /**
@@ -8,7 +10,7 @@ import java.io.Serializable;
  */
 public class ApiResponse<T> implements Serializable {
 
-    private static final long serialVersionUID = -5055727361048438820L;
+    private static final long serialVersionUID = 5748957490055835898L;
 
     public static final int SUCCESS_CODE = 200;
 
@@ -20,21 +22,27 @@ public class ApiResponse<T> implements Serializable {
     private int code;
     private String msg;
     private T data;
+
+    private long timestamp;
+
     public ApiResponse() {
     }
+
     public ApiResponse(T data) {
         this.data = data;
-        this.code=SUCCESS_CODE;
-        this.msg="success";
+        this.code = SUCCESS_CODE;
+        this.msg = SUCCESS_VALUE;
+        this.timestamp = timestamp;
     }
 
     public ApiResponse(String msg, T data) {
         this.msg = msg;
         this.data = data;
+        this.timestamp = timestamp;
     }
 
     public static ApiResponse success(String msg, Object data) {
-        return new ApiResponse(msg,data);
+        return new ApiResponse(msg, data);
     }
 
     public static ApiResponse success(Object data) {
@@ -42,12 +50,13 @@ public class ApiResponse<T> implements Serializable {
     }
 
     public static ApiResponse error(int code, String msg) {
-        return new ApiResponse(code,msg);
+        return new ApiResponse(code, msg);
     }
 
     public ApiResponse(int code, String msg) {
         this.code = code;
         this.msg = msg;
+        this.timestamp = timestamp;
     }
 
     public int getCode() {
@@ -74,12 +83,21 @@ public class ApiResponse<T> implements Serializable {
         this.data = data;
     }
 
+    public long getTimestamp() {
+        return System.currentTimeMillis();
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
     @Override
     public String toString() {
         return "ApiResponse{" +
                 "code=" + code +
                 ", msg='" + msg + '\'' +
                 ", data=" + data +
+                ", timestamp=" + timestamp +
                 '}';
     }
 }
