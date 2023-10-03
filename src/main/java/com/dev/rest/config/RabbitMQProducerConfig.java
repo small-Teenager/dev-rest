5041cd8d5027233e329b01b732d5839c49490e2f
Yaodong Zhang <yaodong199@icloud.com>
2023-10-03 20:07:49 +0800

    publish-subscribe producer

--- src/main/java/com/dev/rest/config/RabbitMQProducerConfig.java
+++ src/main/java/com/dev/rest/config/RabbitMQProducerConfig.java
@@ -1,5 +1,9 @@
 package com.dev.rest.config;
 
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.FanoutExchange;
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
@@ -7,6 +11,59 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQProducerConfig {
+
+    /*------- fanout publish-subscribe ---------*/
+    public static final String PUB_SUB_EXCHANGE_NAME = "fanout-order-exchange";
+    public static final String PUB_SUB_SMS_QUEUE = "sms-fanout-queue";
+    public static final String PUB_SUB_EMAIL_QUEUE = "email-fanout-queue";
+
+    /**
+     * 1.
+     * 声明交换机
+     * @return
+     */
+    @Bean
+    public FanoutExchange fanoutExchange() {
+        /**
+         * FanoutExchange的参数说明:
+         * 1. 交换机名称
+         * 2. 是否持久化 true：持久化，交换机一直保留 false：不持久化，用完就删除
+         * 3. 是否自动删除 false：不自动删除 true：自动删除
+         */
+        return new FanoutExchange(PUB_SUB_EXCHANGE_NAME, true, false);
+    }
+
+    /**
+     * 声明队列
+     * @return
+     */
+    @Bean
+    public Queue smsQueue() {
+        /**
+         * Queue构造函数参数说明
+         * 1. 队列名
+         * 2. 是否持久化 true：持久化 false：不持久化
+         */
+        return new Queue(PUB_SUB_SMS_QUEUE, true);
+    }
+
+    @Bean
+    public Queue emailQueue() {
+        return new Queue(PUB_SUB_EMAIL_QUEUE, true);
+    }
+
+    /**
+     * 队列与交换机绑定
+     */
+    @Bean
+    public Binding smsBinding() {
+        return BindingBuilder.bind(smsQueue()).to(fanoutExchange());
+    }
+
+    @Bean
+    public Binding emailBinding() {
+        return BindingBuilder.bind(emailQueue()).to(fanoutExchange());
+    }
 
     /**
      * 配置生产者rabbitTemplate
--- src/main/java/com/dev/rest/controller/apply/RabbitMQController.java
+++ src/main/java/com/dev/rest/controller/apply/RabbitMQController.java
@@ -1,6 +1,7 @@
 package com.dev.rest.controller.apply;
 
 import com.dev.rest.config.DelayQueueMQConfig;
+import com.dev.rest.config.RabbitMQProducerConfig;
 import com.dev.rest.mq.rabbit.MessageProducer;
 import com.dev.rest.response.ApiResponse;
 import org.slf4j.Logger;
@@ -37,13 +38,22 @@ public class RabbitMQController {
 
     /**
      * rabbitmq delay queue
+     *
      * @param message
      * @return
      */
     @PostMapping("/delay-queue/produce/{message}")
     public ApiResponse<Boolean> producer(@PathVariable(value = "message") @NotNull String message) {
-        log.info("delay queue producer time is:{},message:{}", LocalDateTime.now(),message);
+        log.info("delay queue producer time is:{},message:{}", LocalDateTime.now(), message);
         rabbitTemplate.convertAndSend(DelayQueueMQConfig.SIMPLE_EXCHANGE_NAME, DelayQueueMQConfig.SIMPLE_ROUTING_KEY, message);
+        return ApiResponse.success(true);
+    }
+
+    @PostMapping("/pub-sub/produce/{message}")
+    public ApiResponse<Boolean> pubSub(@PathVariable(value = "message") @NotNull String message) {
+        log.info("publish-subscribe producer time is:{},message:{}", LocalDateTime.now(), message);
+        //由于实现的是fanout模式（广播模式），不需要路由key，所有的消费者都可以进行监听和消费
+        rabbitTemplate.convertAndSend(RabbitMQProducerConfig.PUB_SUB_EXCHANGE_NAME, "", message);
         return ApiResponse.success(true);
     }
 }
