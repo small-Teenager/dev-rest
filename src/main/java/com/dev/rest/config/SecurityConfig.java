5668a9af0f28d2699f9a696f4a1a728f3e4c2ce3
zhangyao <yaodong199@icloud.com>
2024-02-16 22:44:11 +0800

    api key

--- src/main/java/com/dev/rest/config/SecurityConfig.java
+++ src/main/java/com/dev/rest/config/SecurityConfig.java
@@ -15,11 +15,10 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
+        http.csrf()
+                .disable()
                 .authorizeRequests()
                 .antMatchers("/**")
-                .permitAll()
-                .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic()
